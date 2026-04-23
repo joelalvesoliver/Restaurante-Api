5db9c87e3ed98de33bcd775e8554de31613af2c7
Joel Alves <joel.alves.oliver@gmail.com>
2026-04-23 20:40:42 -0300

    upload e download de arquivos

---
+++ Restaurante.Api/DTOs/ArquivoResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurante.Api.DTOs
+{
+    public class ArquivoResponseDTO
+    {
+        public string NomeArmazenado { get; set; } = string.Empty;
+        public string NomeOriginal { get; set; } = string.Empty;
+        public long TamanhoBytes { get; set; }
+        public DateTime DataUpload { get; set; }
+        public string UrlDownload { get; set; } = string.Empty;
+        public string Mensagem { get; set; } = string.Empty;
+    }
+}
